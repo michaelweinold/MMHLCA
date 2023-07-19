51ac9146a9beab2878533974350a968bdb480c19
Michael Weinold <23102087+michaelweinold@users.noreply.github.com>
2023-07-19 10:34:05 +0200

    added debugging file for pyspa

---
+++ dev/debugging_pyspa/debugging_pyspa.py
@@ -0,0 +1,26 @@
+# %%
+
+# pyspa requirements
+import numpy as np
+import pandas as pd
+import scipy as sp
+
+# local import requirements
+import os
+import sys
+
+pyspa_local_path = os.path.join(
+    os.path.expanduser("~"),
+    "github/pyspa"
+)
+sys.path.append(pyspa_local_path)
+
+import pyspa
+
+sc = pyspa.get_spa(
+    target_ID = 70,
+    max_stage = 10,
+    a_matrix = os.path.join(pyspa_local_path, "A_matrix_template.csv"),
+    infosheet = os.path.join(pyspa_local_path, "Infosheet_template.csv"),
+    thresholds = os.path.join(pyspa_local_path, "Thresholds_template.csv"),
+)
